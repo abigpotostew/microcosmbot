dca7ef3834e176af01ddf44e537d7fc9f4b65c7d
Stewart <stew.bracken@gmail.com>
2023-06-18 03:03:53 -0700

    Debug statments

--- packages/bot/src/operations/settings.ts
+++ packages/bot/src/operations/settings.ts
@@ -1,7 +1,10 @@
 import { menuAdminConfig } from '../menus'
 import { MyContext } from '../bot'
+import { logContext } from '../utils'
 
 export const responseSettings = async (ctx: MyContext): Promise<void> => {
+  const cl = logContext(ctx)
+  cl.log('settings!')
   await ctx.reply('Select a group to configure:', {
     reply_markup: menuAdminConfig,
   })
