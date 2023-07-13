b6c0c4c58d4c7fc48df99a8cc83bd090e306a27d
Stewart <stew.bracken@gmail.com>
2023-07-12 22:29:47 -0700

    backfill account usernames

--- scripts/backfill-account-usernames.ts
+++ scripts/backfill-account-usernames.ts
@@ -1,6 +1,27 @@
+import 'dotenv/config'
 import { prismaClient } from '../packages/db'
+import { bot } from '../packages/bot/src/bot'
 
 async function main() {
-  prismaClient().account.findMany()
+  const missingUsernames = await prismaClient().account.findMany({
+    where: {
+      username: null,
+    },
+  })
+  for (let missingUsername of missingUsernames) {
+    const user = await bot.api.getChat(missingUsername.userId)
+    if (user.type === 'private') {
+      const newUsername =
+        user.username || `${user.first_name || ''} ${user.last_name || ''}`
+      await prismaClient().account.update({
+        where: {
+          id: missingUsername.id,
+        },
+        data: {
+          username: newUsername,
+        },
+      })
+    }
+  }
 }
 main()
