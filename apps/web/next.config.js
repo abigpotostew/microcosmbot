427ce404f698e6db36500cb2e884964d19c52753
Stewart <stew.bracken@gmail.com>
2024-03-26 16:08:44 -0700

    maintenance mode

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,4 +12,15 @@ module.exports = withBundleAnalyzer({
     })
     return config
   },
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === '1'
+        ? {
+            source: '/((?!maintenance).*)',
+            destination: '/maintenance',
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean)
+  },
 })
---
+++ apps/web/pages/maintenance.tsx
@@ -0,0 +1,16 @@
+export const Maintenance = () => {
+  return (
+    <div
+      className={
+        'tw-w-full tw-flex tw-flex-col tw-items-center tw-justify-center'
+      }
+    >
+      <h1>Maintenance</h1>
+      <p>
+        Our site is currently undergoing maintenance. Please check back later.
+      </p>
+    </div>
+  )
+}
+
+export default Maintenance
