931b294010e6d310b6f2d91fb16bf4b62aa16727
Stewart <stew.bracken@gmail.com>
2024-04-22 23:03:11 -0700

    Error logging

--- apps/web/pages/api/bot/[bot_id].ts
+++ apps/web/pages/api/bot/[bot_id].ts
@@ -1,6 +1,5 @@
 import { bot } from '@microcosms/bot'
 import { webhookCallback } from 'grammy'
-import { NextApiRequest, NextApiResponse } from 'next'
 import { commands } from '@microcosms/bot'
 import { NextRequest, NextResponse } from 'next/server'
 
@@ -23,15 +22,20 @@ function runMiddleware(req: NextRequest, res: NextResponse, fn: any) {
 bot.use(commands)
 const callback = webhookCallback(bot, 'std/http')
 const handler = async (req: NextRequest, res: NextResponse) => {
-  const botId = req.nextUrl.searchParams.get('bot_id')
-  if (botId !== process.env.TELEGRAM_BOT_KEY) {
-    return NextResponse.json({ message: 'not found' }, { status: 404 })
-  }
+  try {
+    const botId = req.nextUrl.searchParams.get('bot_id')
+    if (botId !== process.env.TELEGRAM_BOT_KEY) {
+      return NextResponse.json({ message: 'not found' }, { status: 404 })
+    }
 
-  if (process.env.NODE_ENV === 'development') {
-    console.log('ding ding')
+    if (process.env.NODE_ENV === 'development') {
+      console.log('ding ding')
+    }
+    await runMiddleware(req, res, callback)
+    // return botWebhook(req, res)
+  } catch (e) {
+    console.error('failed to run', e)
+    return NextResponse.json({ message: 'error' }, { status: 500 })
   }
-  await runMiddleware(req, res, callback)
-  // return botWebhook(req, res)
 }
 export default handler
