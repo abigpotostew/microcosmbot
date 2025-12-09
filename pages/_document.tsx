5d9bc433c6c80b92ed8dff57419801b22d31927d
Stewart <stew.bracken@gmail.com>
2025-12-08 20:36:10 -0800

    stats

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,13 +24,22 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
           <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
           <link rel="manifest" href="/favicon/site.webmanifest" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
+        <script
+          data-goatcounter="https://stats.stewart.codes/count"
+          async
+          src="//stats.stewart.codes/count.js"
+        />
       </Html>
     )
   }
