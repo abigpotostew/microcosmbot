42ef8b29994d47a39c5e4d64b7cf9cabd5d74d37
Stewart <stew.bracken@gmail.com>
2025-12-18 07:02:45 -0800

    next15

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   output: 'standalone',
   transpilePackages: ['@microcosms/db'],
-  experimental: {
-    instrumentationHook: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
